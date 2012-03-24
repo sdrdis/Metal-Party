5f06d72e9ec0755941d91f7f025c310d05c6a6e5
Sébastien Drouyer <sdrdis@gmail.com>
2012-03-24 11:36:27 +0100

    def...

--- metalparty.js
+++ metalparty.js
@@ -23,8 +23,11 @@ metalparty.start = function(){
 	    target = new lime.Layer().setPosition(320,240),
         circle = new lime.Circle().setSize(150,150).setFill(255,150,0),
         lbl = new lime.Label().setSize(160,50).setFontSize(30).setText('TOUCH ME!'),
-        title = new lime.Label().setSize(800,70).setFontSize(60).setText('Now move me around!')
-            .setOpacity(0).setPosition(512,80).setFontColor('#999').setFill(200,100,0,.1);
+        title = new lime.Label().setSize(640,70).setFontSize(60).setText('Now move me around!')
+            .setOpacity(0).setPosition(320,80).setFontColor('#999').setFill(200,100,0,.1);
+            
+           
+
 
 
     //add circle and label to target object
